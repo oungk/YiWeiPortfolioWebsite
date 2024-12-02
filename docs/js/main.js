be23813bd1e82bbf3453b77253a4a034a87e8cac
katherineoung <katherine.oung@gmail.com>
2024-12-02 02:14:31 -0600

    work events bio small tweaks

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,31 +1,5 @@
 'use strict';
-const apiKey= "AIzaSyAqZnZahpWK7uB5FtGUYEEJCBjDO15tnf0"; // API key is not sensitive
-
 /*
-IMPORT GOOGLE SHEET
-*/
-const spreadsheetId = '1RHVT0xtkaSgbpRQ1zsdT4jKfYmhGhftLy9ZrHFgJXkU'; // Replace with your spreadsheet ID
-
-let parsedData;
-
-fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${apiKey}`)
-  .then(response => response.json()) // Get the response as JSON
-  .then(data => {
-    const headers = data.values[0]; // Get the headers from the first row
-    const rows = data.values.slice(1); // Get the remaining rows
-    parsedData = rows.map(row => {
-      const obj = {};
-      headers.forEach((header, index) => {
-        obj[header] = row[index]; // Create an object for each row
-      });
-      return obj; // Return the object for each row
-    });
-
-    console.log(parsedData); // Access the parsed data
-  })
-  .catch(error => console.error('Error fetching the Google Sheets file:', error));
-
-  /*
 EMAIL CONTACT FORM
 */
 
